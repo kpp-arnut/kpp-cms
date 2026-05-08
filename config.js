36d21a7d69d0477a8fea0219b002b0900ce6510b
ARNUT KLANGPRAPUN <136555438+kpp-arnut@users.noreply.github.com>
2026-05-08 10:48:34 +0700

    Update config.js to use environment variables
    Refactor configuration to use environment variables.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// CONFIGURATION
-const SUPABASE_URL = 'https://gfikiopjopxhogrhpcpx.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_6pccrfn1Ng_FXHugztRsmw__kW_TtAL';
-const GOOGLE_CLIENT_ID = '292294403643-n756epva6fet5p7clkao3r2damoeai0r.apps.googleusercontent.com';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-const _sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+export const _sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+export { GOOGLE_CLIENT_ID };
