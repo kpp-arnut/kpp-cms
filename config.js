7a406b90a1276c9c0a8735a691130e2bfe82a0a9
ARNUT KLANGPRAPUN <136555438+kpp-arnut@users.noreply.github.com>
2026-05-08 11:02:32 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
-export const _sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-export { GOOGLE_CLIENT_ID };
