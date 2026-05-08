2e0d4b7622594349482b860a9fe07c53950d5958
ARNUT KLANGPRAPUN <136555438+kpp-arnut@users.noreply.github.com>
2026-05-08 12:13:56 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// CONFIGURATION
-const SUPABASE_URL = 'https://gfikiopjopxhogrhpcpx.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_6pccrfn1Ng_FXHugztRsmw__kW_TtAL';
-const GOOGLE_CLIENT_ID = '292294403643-n756epva6fet5p7clkao3r2damoeai0r.apps.googleusercontent.com';
-
-const _sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
